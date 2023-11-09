ee0997a731aeeef6a14ece8d8f7bcbb540ad94b1
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-09 13:31:33 +0000

    Criação da `lista` e endpoint `READ ALL [GET] /item`

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!")
 })
 
+const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
+
+app.get("/item", function (req, res) {
+  res.send(lista)
+})
+
 app.listen(3000)
